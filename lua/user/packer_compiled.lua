aa25968ae317a837ce69f4a96897c1273d87fb29
Ardavan Behnia <aba122@sfu.ca>
2023-02-21 19:21:58 -0800

    Add new Packer compiled.

--- lua/user/packer_compiled.lua
+++ lua/user/packer_compiled.lua
@@ -132,6 +132,16 @@ _G.packer_plugins = {
     path = "/home/ard/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  ["mason-lspconfig.nvim"] = {
+    loaded = true,
+    path = "/home/ard/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
+  ["mason.nvim"] = {
+    loaded = true,
+    path = "/home/ard/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
+  },
   neogit = {
     loaded = true,
     path = "/home/ard/.local/share/nvim/site/pack/packer/start/neogit",
@@ -156,11 +166,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/ard/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
-  },
-  ["nvim-lsp-installer"] = {
-    loaded = true,
-    path = "/home/ard/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
-    url = "https://github.com/williamboman/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
